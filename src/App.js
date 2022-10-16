94cfd2379ad2eae30207cdab610c2b7ef41bbd1e
abhi nandan <abhinandan8891@gmail.com>
2022-10-16 21:34:17 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+            <Route exact path="/home" element={<Home/>} />
+            <Route exact path="/about" element={<About />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Herobanner from './Components/Herobanner';
+
+function Home() {
+  return (
+   <>
+   <Herobanner/>
+   </> 
+  )
+}
+
+export default Home
